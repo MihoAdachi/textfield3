19c81a9ef6d82dafa790c54fb4d64c6ed3eea2e7
ponta01 <ponta01@stitch.lassic.local>
2014-07-25 20:00:43 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textform3::Application.config.secret_token = 'e54eead53fd081ef315f9cf07355a935c9646de3aef276d639dba70b8883e6a7d0c6a721edac3e0c0072a20ea577f184f69eeb6c3c41803d8b5faab98ec77af0'
